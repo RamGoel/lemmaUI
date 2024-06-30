bca037d10b368f7717b41b241259aad690ef9d25
Ram Goel <rgoel766@gmail.com>
2024-06-30 08:12:13 +0530

    fix: result height

--- components/result.tsx
+++ components/result.tsx
@@ -1,7 +1,12 @@
 const ResultUI = ({ htmlCode }: { htmlCode: string }) => {
     return (
-        <div className="flex items-center justify-center flex-1">
-            <iframe src="/result.html" width={700} className="h-fit"></iframe>
+        <div className="flex items-center !bg-white justify-center flex-1">
+            <iframe
+                src="/result.html"
+                width={700}
+                height={700}
+                className=""
+            ></iframe>
         </div>
     )
 }
